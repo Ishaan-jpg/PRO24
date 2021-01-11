2f8777a755d57cef48306586f2fba73efb1a9701
Ishaan-jpg <74717638+Ishaan-jpg@users.noreply.github.com>
2021-01-11 12:11:49 +0530

    Add files via upload

--- paper.js
+++ paper.js
@@ -4,7 +4,7 @@ class Paper{
             isStatic:false,
             restitution:0.3,
             friction:0.5,
-            density:1.2
+            density:5.5
         }
         this.radius = radius
         this.x=x
